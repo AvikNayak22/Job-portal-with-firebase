20581c3bb05fdfe5c2f5a1e5a0f6ccdea72a7ec8
AvikNayak <nayakavik19@gmail.com>
2024-05-27 17:17:39 +0530

    first commit

---
+++ src/components/Navbar/index.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Navbar = () => {
+  return (
+    <div className="h-20 flex items-center w-full text-white ">
+      <div className="text-3xl pl-20 font-bold ">Joboard.</div>
+    </div>
+  );
+};
+
+export default Navbar;
